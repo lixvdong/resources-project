9a04d059751ea524933f188902786e687ac4f284
lixvdong <lixvdon@qq.com>
2022-07-25 19:51:43 +0800

    完成了页面全屏变化

--- src/components/FullScreen/index.vue
+++ src/components/FullScreen/index.vue
@@ -1,20 +1,45 @@
 <template>
   <!-- 放置一个图标 -->
   <div class="screenWrapper">
-    <svg-icon
-      icon-class="fullscreen"
-      class="fullscreen"
-      @click="toggleScreen"
-    />
+    <svg-icon :icon-class="!isScreenFull?'fullscreen':'exit-fullscreen'" class="fullscreen" @click="toggleScreen" />
   </div>
 </template>
 
 <script>
+import screenfull from 'screenfull'
 export default {
   name: 'ScreenFull',
+  data() {
+    return {
+      isScreenFull: false
+    }
+  },
+  mounted() {
+  //   // 监听窗口变化
+  //   document.addEventListener('fullscreenchange', e => {
+  //     const isFull = document.fullscreenElement
+  //     if (!isFull) {
+  //       this.isScreenFull = false
+  //     }
+  //   })
+  // 使用插件监听窗口变化
+    screenfull.on('change', () => {
+      this.isFull = screenfull.isFullscreen
+    })
+  },
   methods: {
+    // 手动点击窗口变化
+    // toggleScreen() {
+    //   if (!this.isScreenFull) {
+    //     document.documentElement.requestFullscreen()
+    //   } else {
+    //     document.exitFullscreen()
+    //   }
+    //   this.isScreenFull = !this.isScreenFull
+    // }
+    // 使用插件，变化窗口
     toggleScreen() {
-
+      screenfull.toggle()
     }
   }
 }
